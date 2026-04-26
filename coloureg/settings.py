f7ce1d06a435ed53d003f5deaf442efb166f139b
freed-man <rolandk@proton.me>
2026-04-26 21:06:31 +0100

    .

--- coloureg/settings.py
+++ coloureg/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEVELOPMENT', '') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com', '.coloureg.com',]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com', '.coloureg.com', '.coloureg.co.uk']
 
 # Application definition
 
