5cd11a5663a141a486888e6941252ee348bfbd1f
freed-man <rolandk@proton.me>
2026-04-27 02:24:26 +0100

    .

--- coloureg/urls.py
+++ coloureg/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
+from django.views.generic import TemplateView
 from lookup.sitemaps import StaticViewSitemap
 
 sitemaps = {
@@ -11,4 +12,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('lookup.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
